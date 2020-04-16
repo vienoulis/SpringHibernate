7587144e85f3aff1dc256b03cba625052be9e729
Лукьянов Роман Андреевич <vienoulis@gmail.com>
2020-04-16 19:15:06 +0300

    Done

---
+++ src/main/java/hiber/model/Car.java
@@ -0,0 +1,61 @@
+package hiber.model;
+
+import org.springframework.stereotype.Component;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cars")
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "series")
+    private int series;
+
+    @OneToOne(mappedBy = "car")
+    private User user;
+
+    public Car() {
+    }
+
+    public Car(String name, int series) {
+        this.name = name;
+        this.series = series;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + series + " series";
+    }
+}
